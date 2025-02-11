10e4b6ec8e52fbd9549646f1696c8129cba00289
Nils Aufschläger <mail@nilsauf.de>
2025-02-11 22:23:16 +0100

    remove unused exception types

--- FTMS.NET.Contracts/Exceptions/FitnessMachineDataCreationException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FTMS.NET.Exceptions;
-using System;
-
-public class FitnessMachineDataCreationException(Exception ex) : Exception(ExceptionMessage, ex)
-{
-	private const string ExceptionMessage = "The FitnessMachineData could not be created! " +
-		"The data type needs to derive from FitnessMachineData<TData> and " +
-		"needs to implement a constructur with a single IObservable<byte[]> parameter.";
-}
