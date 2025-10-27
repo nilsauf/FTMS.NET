a9151c6dac8c634fee38ec868039e90154b4afd9
Nils Aufschläger <mail@nilsauf.de>
2025-10-27 19:56:09 +0100

    remove ToDebugString

--- FTMS.NET.Contracts/Utils/ByteExtensions.cs
+++ FTMS.NET.Contracts/Utils/ByteExtensions.cs
@@ -1,4 +1,5 @@
 ﻿namespace FTMS.NET.Utils;
+
 public static class ByteExtensions
 {
 	public static bool IsBitSet(this byte[] data, int pos)
@@ -17,9 +18,4 @@ public static class ByteExtensions
 
 	public static bool IsBitSet(this byte b, int pos)
 		=> (b & 1 << pos) != 0;
-
-	public static string ToDebugString(this byte[]? data)
-		=> data is null ?
-			"No Data" :
-			string.Join(" ", data.Reverse().Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
 }
