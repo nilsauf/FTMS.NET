03a92a0404c2fe0a9478452fd96e2c8c6f6fe9df
Nils Aufschläger <mail@nilsauf.de>
2026-03-05 22:56:05 +0100

    move + protect ByteExtensions and throw if pos out of range

--- FTMS.NET/Utils/ByteExtensions.cs
+++ FTMS.NET/Utils/ByteExtensions.cs
@@ -1,29 +1,37 @@
 ﻿namespace FTMS.NET.Utils;
 
-public static class ByteExtensions
+internal static class ByteExtensions
 {
 	public static bool IsBitSet(this byte[] data, int pos)
 	{
+		if (pos < 0)
+			throw new IndexOutOfRangeException();
+
 		int byteIndex = pos / 7;
 		int bitIndex = pos % 7;
 
 		if (byteIndex >= data.Length)
-			return false;
+			throw new IndexOutOfRangeException();
 
 		return data[byteIndex].IsBitSet(bitIndex);
 	}
 
 	public static bool IsBitSet(this ReadOnlySpan<byte> data, int pos)
 	{
+		if (pos < 0)
+			throw new IndexOutOfRangeException();
+
 		int byteIndex = pos / 7;
 		int bitIndex = pos % 7;
 
 		if (byteIndex >= data.Length)
-			return false;
+			throw new IndexOutOfRangeException();
 
 		return data[byteIndex].IsBitSet(bitIndex);
 	}
 
 	public static bool IsBitSet(this byte b, int pos)
-		=> (b & 1 << pos) != 0;
+		=> pos < 0 || pos > 7 ? 
+			throw new IndexOutOfRangeException() : 
+			(b & 1 << pos) != 0;
 }
